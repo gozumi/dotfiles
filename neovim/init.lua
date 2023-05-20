517b7a5cbf12a26cb8008c9cfe8f5cff9f485a2f
Ian Anderson <ian@gozumi.com>
2023-05-20 22:30:34 +0000

    Add the neovim init.lua file to the project.

---
+++ neovim/init.lua
@@ -0,0 +1,35 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  "folke/tokyonight.nvim",
+  "nvim-lualine/lualine.nvim"
+})
+
+-- vim.cmd('colorscheme tokyonight')
+
+
+
+vim.opt.showmode = false
+
+require('lualine').setup({
+  options = {
+    theme = 'onedark',
+    icons_enabled = true,
+    component_separators = '|',
+    section_separators = '',
+    disabled_filetypes = {
+      statusline = {'NvimTree'}
+    }
+  },
+})
