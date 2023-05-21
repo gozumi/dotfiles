cc0d256d19d6ae7360f5a2ea448f086b0cbe58ce
Ian Anderson <ian@gozumi.com>
2023-05-21 22:19:52 +0100

    Update the initial init.lua file.

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,4 +1,5 @@
 vim.wo.relativenumber = true
+vim.wo.number = true 
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -15,7 +16,9 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   "folke/tokyonight.nvim",
-  "nvim-lualine/lualine.nvim"
+  "nvim-lualine/lualine.nvim",
+  "lukas-reineke/indent-blankline.nvim",
+  "nvim-treesitter/nvim-treesitter"
 })
 
 -- vim.cmd('colorscheme tokyonight')
@@ -35,3 +38,26 @@ require('lualine').setup({
     }
   },
 })
+
+require('indent_blankline').setup({
+  char = '_',
+  show_trailing_blankline_indent = false,
+  show_first_indent_level = false,
+  use_treesitter = true,
+  show_current_context = true 
+})
+
+require('nvim-treesitter.configs').setup({
+  highlight = {
+    enable = true,
+  },
+  ensure_installed = {
+    'javascript',
+    'typescript',
+    'tsx',
+    'css',
+    'json',
+    'lua',
+  },
+})
+
