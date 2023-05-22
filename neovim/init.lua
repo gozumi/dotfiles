3f2f08ebbb70b80462bc306f3a6b00dec5da93aa
Ian Anderson <ian@gozumi.com>
2023-05-22 22:17:38 +0000

    Update the neovim init.lua file.

--- neovim/init.lua
+++ neovim/init.lua
@@ -18,12 +18,15 @@ require("lazy").setup({
   "folke/tokyonight.nvim",
   "nvim-lualine/lualine.nvim",
   "lukas-reineke/indent-blankline.nvim",
-  "nvim-treesitter/nvim-treesitter"
+  "nvim-treesitter/nvim-treesitter",
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  "numToStr/Comment.nvim",
+  "kyazdani42/nvim-tree.lua",
+  "nvim-telescope/telescope.nvim",
+  "nvim-lua/plenary.nvim"
 })
 
 -- vim.cmd('colorscheme tokyonight')
-
-
 
 vim.opt.showmode = false
 
@@ -60,4 +63,52 @@ require('nvim-treesitter.configs').setup({
     'lua',
   },
 })
+
+require('nvim-treesitter.configs').setup({
+  highlight = {
+    enable = true,
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      }
+    },
+  },
+  ensure_installed = {
+    --- parsers....
+  },
+})
+
+require('Comment').setup({})
+
+require('nvim-tree').setup({
+  hijack_cursor = true,
+  on_attach = function(bufnr)
+    local bufmap = function(lhs, rhs, desc)
+      vim.keymap.set('n', lhs, rhs, {buffer = bufnr, desc = desc})
+    end
+
+    -- See :help nvim-tree.api
+    local api = require('nvim-tree.api')
+   
+    bufmap('L', api.node.open.edit, 'Expand folder or go to file')
+    bufmap('H', api.node.navigate.parent_close, 'Close parent folder')
+    bufmap('gh', api.tree.toggle_hidden_filter, 'Toggle hidden files')
+  end
+})
+
+vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<cr>')
+
+vim.keymap.set('n', '<leader><space>', '<cmd>Telescope buffers<cr>')
+vim.keymap.set('n', '<leader>?', '<cmd>Telescope oldfiles<cr>')
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
+vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>')
+vim.keymap.set('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
 
