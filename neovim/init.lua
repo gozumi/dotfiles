2a924cfe5d76a8d0e78fabd4649af7cb4ba735fb
Ian Anderson <ian@gozumi.com>
2023-05-26 00:55:01 +0100

    Fix the neovim forgit integration.

--- neovim/init.lua
+++ neovim/init.lua
@@ -164,3 +164,27 @@ require'forgit'.setup({
   cmds_list = {} -- additional commands to show in Forgit command list
   --  e.g. cmd_list = {text = 'Gs get_hunks', cmd = 'Gitsigns get_hunks'}
 })
+
+-- default mapping
+maps = {
+  close_view = '<C-e>',
+  send_qf = '<C-q>',
+  save = '<C-s>',
+  jump_to_list = '<C-w>k',
+  jump_to_preview = '<C-w>j',
+  prev = '<C-p>',
+  next = '<C-n>',
+  pageup = '<C-b>',
+  pagedown = '<C-f>',
+  confirm = '<C-o>',
+  split = '<C-s>',
+  vsplit = '<C-v>',
+  tabnew = '<C-t>',
+}
+
+  --
+require('guihua.maps').setup({
+  maps = {
+    close_view = '<C-x>',
+  }
+})
